c92a0f509dac6514dc505dc18ac7c3c77005a712
Faizan kaishar <115065680+faiz1487@users.noreply.github.com>
2025-12-30 10:30:05 +0530

    Delete write_file.py

--- write_file.py
+++
@@ -1,6 +0,0 @@
-file = open("sample.txt", "w")
-file.write("Hello, this is a sample text file.\n")
-file.write("Python file handling is easy to learn.")
-file.close()
-
-print("Data written to file successfully.")
